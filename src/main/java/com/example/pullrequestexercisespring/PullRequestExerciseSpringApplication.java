40ad516456f71a67b62ff1fc72434f66cd07a34c
AndersNystrupJ <anje0008@stud.kea.dk>
2024-09-17 13:34:44 +0200

    Initial Commit

---
+++ src/main/java/com/example/pullrequestexercisespring/PullRequestExerciseSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.pullrequestexercisespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullRequestExerciseSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PullRequestExerciseSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pullrequestexercisespring/PullRequestExerciseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pullrequestexercisespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullRequestExerciseSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
